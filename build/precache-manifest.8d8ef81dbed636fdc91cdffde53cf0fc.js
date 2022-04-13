6f3b6086ed0dd43c295186a941769d49e1a1d925
gurmit <gurmit.randhawa07@gmail.com>
2022-04-13 10:45:18 +0100

    all done

---
+++ build/precache-manifest.8d8ef81dbed636fdc91cdffde53cf0fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a94b07c2f95d14f93820d5b52d3f612",
+    "url": "/index.html"
+  },
+  {
+    "revision": "160888f917fedbbeb129",
+    "url": "/static/css/main.93950f30.chunk.css"
+  },
+  {
+    "revision": "4b5a0659b690fe5c44a5",
+    "url": "/static/js/2.107f0c64.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.107f0c64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "160888f917fedbbeb129",
+    "url": "/static/js/main.55e6699f.chunk.js"
+  },
+  {
+    "revision": "f55b84c65c092ec71c90",
+    "url": "/static/js/runtime-main.bcab16a1.js"
+  }
+]);
